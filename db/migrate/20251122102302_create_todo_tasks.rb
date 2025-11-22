4389fb73182ad73d0162bfd73a2408ed3e3ad11d
kouheigit <ookawakouhei777@gmail.com>
2025-11-22 19:36:11 +0900

    feat:Todotask migrationファイルの実装

--- db/migrate/20251122102302_create_todo_tasks.rb
+++ db/migrate/20251122102302_create_todo_tasks.rb
@@ -1,14 +1,14 @@
 class CreateTodoTasks < ActiveRecord::Migration[7.1]
-  def change
-    create_table :todo_tasks do |t|
-    t.references :todo_user, null: false, foreign_key: true
-     t.string :title, null: false
-     t.text :description
-     t.datetime :due_date
-     t.references :todo_status, null: false, foreign_key: true
-     t.references :todo_priority, null: false, foreign_key: true
+ def change
+     create_table :todo_tasks do |t|
+       t.references :todo_user, null: false, foreign_key: true
+       t.string :title, null: false
+       t.text :description
+       t.datetime :due_date
+       t.references :todo_status, null: false, foreign_key: true
+       t.references :todo_priority, null: false, foreign_key: true
 
-     t.timestamps
+       t.timestamps
     end
   end
 end
