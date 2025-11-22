107eafcceeb8fe20f1f05655b0917bb878f5c681
kouheigit <ookawakouhei777@gmail.com>
2025-11-22 18:51:20 +0900

    feat:migrateファイルの追加

--- db/migrate/20251122093836_create_todo_statuses.rb
+++ db/migrate/20251122093836_create_todo_statuses.rb
@@ -1,10 +1,12 @@
 class CreateTodoStatuses < ActiveRecord::Migration[7.1]
   def change
     create_table :todo_statuses do |t|
-      t.string :name
-      t.string :label
+      t.string :name, null: false
+      t.string :label, null: false
 
       t.timestamps
     end
+
+    add_index :todo_statuses, :name, unique: true
   end
 end
