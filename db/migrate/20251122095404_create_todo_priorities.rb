1a662ebaaa3e318df0767c3f909bf926ad5e7d7c
kouheigit <ookawakouhei777@gmail.com>
2025-11-22 19:06:27 +0900

    feat:TodoPriorityの実装

--- db/migrate/20251122095404_create_todo_priorities.rb
+++ db/migrate/20251122095404_create_todo_priorities.rb
@@ -1,11 +1,13 @@
 class CreateTodoPriorities < ActiveRecord::Migration[7.1]
   def change
     create_table :todo_priorities do |t|
-      t.string :name
-      t.string :label
-      t.integer :sort_order
+      t.string :name, null: false
+      t.string :label, null: false
+      t.integer :sort_order,default: 0
 
       t.timestamps
     end
+
+    add_index :todo_priorities, :name, unique: true
   end
 end
