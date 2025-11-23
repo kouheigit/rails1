3e9395be15227705e711082258c4fbb383ddd28a
kouheigit <ookawakouhei777@gmail.com>
2025-11-23 16:24:34 +0900

    feat:Todo_priorityの実装

--- app/models/todo_priority.rb
+++ app/models/todo_priority.rb
@@ -1,2 +1,11 @@
 class TodoPriority < ApplicationRecord
+  #バリデーション
+  validates :name, presence: true, uniqueness: true
+  validates :label, presence: true
+
+  #リレーション
+  has_many :todo_tasks, dependent: :destroy
+
+  # ソート順で並び替え
+  scope :ordered, -> { order(:sort_order) }
 end
