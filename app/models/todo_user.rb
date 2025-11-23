9f2d0df7b7639999483290a2ab9e9ed4dc06a2c2
kouheigit <ookawakouhei777@gmail.com>
2025-11-23 14:49:00 +0900

    feat:TodoUserモデルの実装

--- app/models/todo_user.rb
+++ app/models/todo_user.rb
@@ -1,47 +1,12 @@
 class TodoUser < ApplicationRecord
+  # パスワードのハッシュ化
+  has_secure_password
+
+  #バリデーション
+  validates :name, presence: true
+  validates :email presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
 
 
 end
 
-
-
-```ruby
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable; // 認証ユーザーとして使う
-use Illuminate\Notifications\Notifiable;
-
-class TodoUser extends Authenticatable
-{
-    use HasFactory, Notifiable;
-
-    // 保存を許可するカラム
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    // パスワードは自動でハッシュ化
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    // 型キャストなど（必要なら）
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    /**
-     * リレーション: 1人のユーザーは複数の TodoTask を持つ
-     */
-    public function todoTasks()
-    {
-        return $this->hasMany(TodoTask::class);
-    }
-}
-```
