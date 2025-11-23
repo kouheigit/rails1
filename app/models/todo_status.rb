18cd2fae4b8eab2568bc40c693e4f4650abf4046
kouheigit <ookawakouhei777@gmail.com>
2025-11-23 15:36:37 +0900

    feat:Todo_statusモデルの実装

--- app/models/todo_status.rb
+++ app/models/todo_status.rb
@@ -1,2 +1,14 @@
 class TodoStatus < ApplicationRecord
+ #バリデーション(バリデーションはRailsではModelに記述する)
+ validates :name, presence: true, uniqueness: true
+ validates :label,presence: true
+
+ #リレーション
+ has_many :todo_tasks, dependent: :destroy
 end
+
+
+
+
+
+```
