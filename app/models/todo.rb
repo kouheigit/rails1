f7f6425be76bdbb1103c6d9392f91bd4dc59b46c
kouheigit <ookawakouhei777@gmail.com>
2025-11-23 17:17:28 +0900

    fix:Todoモデルの修正

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,12 @@
 class Todo < ApplicationRecord
+  # バリデーション
+  validates :title, presence: true
+  validates :user_id, presence: true
+
+  # リレーション
   belongs_to :user
+
+  # スコープ
+  scope :recent, -> { order(created_at: :desc) }
+  scope :by_user, ->(user_id) { where(user_id: user_id) }
 end
