57c0f5fb2f8e16a99bc07a4cf4808d8e090fbd59
kouheigit <ookawakouhei777@gmail.com>
2025-11-23 16:05:49 +0900

    feat:TodoUserモデルの実装

--- app/models/todo_status.rb
+++ app/models/todo_status.rb
@@ -10,5 +10,15 @@ end
 
 
 
+#### app/models/todo_status.rb
 
+```ruby
+class TodoStatus < ApplicationRecord
+  # バリデーション
+  validates :name, presence: true, uniqueness: true
+  validates :label, presence: true
+
+  # リレーション
+  has_many :todo_tasks, dependent: :destroy
+end
 ```
