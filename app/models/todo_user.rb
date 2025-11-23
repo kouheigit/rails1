5a857f35c0fc763f83bd382aa1dd6f3de28f4523
kouheigit <ookawakouhei777@gmail.com>
2025-11-23 15:09:24 +0900

    feat:TodoUserモデルの実装

--- app/models/todo_user.rb
+++ app/models/todo_user.rb
@@ -6,7 +6,38 @@ class TodoUser < ApplicationRecord
   validates :name, presence: true
   validates :email presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
-
-
+  #リレーション
+  has_many :todo_tasks, dependent: :destroy
 end
 
+
+```ruby
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class TodoUser extends Authenticatable
+{
+    use HasFactory;
+
+    /**
+     * has_secure_password 相当
+     * パスワードを自動でハッシュ化
+     */
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+
+    /**
+     * has_many :todo_tasks, dependent: :destroy 相当
+     */
+    public function todoTasks()
+    {
+        return $this->hasMany(TodoTask::class);
+    }
+}
+```
