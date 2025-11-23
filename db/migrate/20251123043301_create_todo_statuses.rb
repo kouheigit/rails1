f83d34d14c35d6d06534e429bee0d3bda2143da2
kouheigit <ookawakouhei777@gmail.com>
2025-11-23 17:15:10 +0900

    feat:migrationの実行

--- db/migrate/20251123043301_create_todo_statuses.rb
+++ db/migrate/20251123043301_create_todo_statuses.rb
@@ -1,13 +1,15 @@
 class CreateTodoStatuses < ActiveRecord::Migration[7.1]
   def change
-    create_table :todo_statuses do |t|
-     t.string :name, null: false
-     t.string :label, null: false
+    unless table_exists?(:todo_statuses)
+      create_table :todo_statuses do |t|
+        t.string :name, null: false
+        t.string :label, null: false
 
-      t.timestamps
-    end
+        t.timestamps
+      end
 
       add_index :todo_statuses, :name, unique: true
+    end
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_22_102302) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_23_043301) do
   create_table "todo_priorities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "label", null: false
