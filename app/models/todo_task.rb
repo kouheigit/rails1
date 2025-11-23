0316417f49a52735b54a0f82f7f4f197cebe3383
kouheigit <ookawakouhei777@gmail.com>
2025-11-23 16:55:18 +0900

    feat:TodoTaskModeljissou

--- app/models/todo_task.rb
+++ app/models/todo_task.rb
@@ -1,6 +1,11 @@
 class TodoTask < ApplicationRecord
-#バリデーション
+ #バリデーション
+ validates :title, presence: true
+ validates :todo_user_id, presence: true
+ validates :todo_status_id,presence: true
+ validates :todo_priority_id, presence: true
+
+
 
 end
-
 
