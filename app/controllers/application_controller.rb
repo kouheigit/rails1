8db9d16767ad28e49d3682777440170e5b62a9a7
kouheigit <ookawakouhei777@gmail.com>
2025-11-26 17:58:04 +0900

    feat:appliaction_controllerの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,17 @@ class ApplicationController < ActionController::Base
 protect_from_forgery with: :exception
 
 #現在のTodoUserを取得
-def current_todo_user
- @current_todo_user ||= TodoUSer.find(session[:todo_user_id]) if session[:todo_user_id]
-end
+ def current_todo_user
+  @current_todo_user ||= TodoUSer.find(session[:todo_user_id]) if session[:todo_user_id]
+ end
+ helper_method :current_todo_user
+
+ #ログインしているかチェック
+ def todo_user_logged_in
+  !!current_todo_user
+ end
+ helper_method :todo_user_logged_in?
+
 end
 
 
