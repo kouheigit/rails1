28a634b59fd621cf74906c47bd30810ca01c0293
kouheigit <ookawakouhei777@gmail.com>
2025-11-27 20:23:23 +0900

    feat:todo_user_controllerの実装

--- app/controllers/todo_users_controller.rb
+++ app/controllers/todo_users_controller.rb
@@ -5,4 +5,24 @@ class TodoUsersController < ApplicationController
   @todo_user = TodoUser.new
  end
 
+ def new
+  @todo_user = TodoUser.new
+ end
+
+ def create
+  @todo_user = TodoUser.new(todo_user_params)
+  if @todo_user.save
+    session[:todo_user_id] = @todo_user.id
+    redirect_to todo_task_path, notice: '登録が完了しました'
+  else
+   render :new, status: unprocessable_entity
+   end
+ end
+
+ private
+
+ def todo_user_params
+  params.require(:todo_user).permit(:name,:email, :password, :password_confirmation)
+ end
+end
 
