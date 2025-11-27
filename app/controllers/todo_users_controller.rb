cdafb4866bc1fdedc40311a677c5913a3a14a302
kouheigit <ookawakouhei777@gmail.com>
2025-11-27 20:36:32 +0900

    feat:todo_user_controllerの実装

--- app/controllers/todo_users_controller.rb
+++ app/controllers/todo_users_controller.rb
@@ -1,28 +1,38 @@
 class TodoUsersController < ApplicationController
- skip_before_action :require_todo_user_login, only: [:new, :create]
 
- def new
-  @todo_user = TodoUser.new
- end
+  skip_before_action :require_todo_user_login, only: [:new, :create]
 
- def new
-  @todo_user = TodoUser.new
- end
+  def new
 
- def create
-  @todo_user = TodoUser.new(todo_user_params)
-  if @todo_user.save
-    session[:todo_user_id] = @todo_user.id
-    redirect_to todo_task_path, notice: '登録が完了しました'
-  else
-   render :new, status: unprocessable_entity
-   end
- end
+    @todo_user = TodoUser.new
 
- private
+  end
 
- def todo_user_params
-  params.require(:todo_user).permit(:name,:email, :password, :password_confirmation)
- end
+  def create
+
+    @todo_user = TodoUser.new(todo_user_params)
+
+    if @todo_user.save
+
+      session[:todo_user_id] = @todo_user.id
+
+      redirect_to todo_tasks_path, notice: '登録が完了しました'
+
+    else
+
+      render :new, status: :unprocessable_entity
+
+    end
+
+  end
+
+  private
+
+  def todo_user_params
+
+    params.require(:todo_user).permit(:name, :email, :password, :password_confirmation)
+
+  end
+
 end
 
